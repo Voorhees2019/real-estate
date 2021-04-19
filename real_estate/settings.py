ae7a94f437405744bea07f6d2eb7f257f517ce31
Artur Petryk <petrikartur@gmail.com>
2021-04-19 20:57:04 +0300

    changed templates dirs

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -126,11 +126,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'real_estate/static'
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'real_estate/static'
+# ]
 
 # Media Folder Settings
 MEDIA_ROOT = BASE_DIR / 'media'
